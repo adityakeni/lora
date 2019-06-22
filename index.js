d2fa70acb65d494c2567ecb0e62e877a1a23b757
Aditya Keni <adity.keni@gmail.com>
2019-06-22 11:07:42 +0530

    fixing errors

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const app = express()
 const port = process.env.PORT || 3000
 
 //Define paths for express configuration
-const publicDirectoryPath = path.join(__dirname, '/public')
-const viewPath = path.join(__dirname, '/templates/views')
-const partialsPath = path.join(__dirname, '/templates/partials')
+// const publicDirectoryPath = path.join(__dirname, '/public')
+// const viewPath = path.join(__dirname, '/templates/views')
+// const partialsPath = path.join(__dirname, '/templates/partials')
 
-//Setup for static directory to serve
-app.use(express.static(publicDirectoryPath))
+// //Setup for static directory to serve
+// app.use(express.static(publicDirectoryPath))
 
-//setup handlebar engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewPath)
-hbs.registerPartials(partialsPath)
+// //setup handlebar engine and views location
+// app.set('view engine', 'hbs')
+// app.set('views', viewPath)
+// hbs.registerPartials(partialsPath)
 
 
 app.use(bodyParser.json())
@@ -36,55 +36,23 @@ app.post('/', (request, response) => {
     
 })
 
-function base64ToHex(str) {
-  for (var i = 0, bin = atob(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
-      let tmp = bin.charCodeAt(i).toString(16);
-      if (tmp.length === 1) tmp = "0" + tmp;
-      hex[hex.length] = tmp;
-  }
-  return hex.join(" ");
-}
+// function base64ToHex(str) {
+//   for (var i = 0, bin = atob(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
+//       let tmp = bin.charCodeAt(i).toString(16);
+//       if (tmp.length === 1) tmp = "0" + tmp;
+//       hex[hex.length] = tmp;
+//   }
+//   return hex.join(" ");
+// }
 
 app.get('/', (req, res) => {
-    res.render('index', {
-      title: 'Lora',
-      name: 'Aditya Keni'
-    })
     res.json(data)
-    let d = base64ToHex( data.data );
-    res.send(d.toUpperCase())
+    // let d = base64ToHex( data.data );
+    // res.send(d.toUpperCase())
 }) 
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Aditya Keni'
-    })
-})
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help Page',
-        helpText: 'Some helpful text',
-        name: 'Aditya Keni'
-    })
-})
 
-app.get('/help/*', (req, res) => {
-  res.render('404', {
-      title: 404,
-      errorMessage: 'help article not found!',
-      name: 'Aditya Keni'
-  })
-})
-
-app.get('*', (req, res) => {
-  res.render('404', {
-      title: 404,
-      errorMessage: 'Page not found!',
-      name: 'Aditya Keni'
-  })
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
