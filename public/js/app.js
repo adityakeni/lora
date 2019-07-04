292d805e71e50e8a98d555f640f7e307c7011798
Aditya Keni <adity.keni@gmail.com>
2019-07-04 11:55:18 +0530

    base64 to hex conversion

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
+console.log('Client side javascript')
+
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+const messageThree = document.querySelector('#message-3')
+const messageFour = document.querySelector('#message-4')
 
 fetch('/lora').then((response) => {
     messageOne.textContent = 'JSON Data'
+    messageThree.textContent = 'Hex Data'
     response.json().then((data) => {
         if (data.error) {
-            messageTwo.textContent = data.error
+            messageTwo.textContent = 'Data is not accessible!'
         } else {
             messageTwo.textContent = JSON.stringify(data)
+            const rawData = data.data
+            const buffer = Buffer.from(rawData, 'base64')
+            const bufString = buffer.toString('hex')
+            messageFour.textContent = bufString
         }
     })
 })
